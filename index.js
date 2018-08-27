2cb2dd564d351a82f45c42eb597193cbdd892ce1
Carina Di Domenico <carinadidomenico@gmail.com>
2018-08-27 08:55:34 -0300

    backend primeiros passos

---
+++ index.js
@@ -0,0 +1,15 @@
+let entrada1 = Number(process.argv[2]);
+let entrada2 =Number(process.argv [3]);
+
+if(!entrada1 || !entrada2){
+    console.log('É necessário informar dois valores');
+}else{
+    let soma= entrada1+entrada2;
+
+}
+
+
+
+console.log (soma); 
+
+//console.log(process.argv);
